10bb4ab7b68099c9c127f8bcb624d21406ff263e
juliano340 <juliano340@gmail.com>
2022-07-16 01:36:02 -0300

    Adcionada exclusão de filme salvo

--- src/Pages/Favoritos/index.js
+++ src/Pages/Favoritos/index.js
@@ -16,10 +16,23 @@ function Favoritos() {
 
     }, [])
 
+    function excluirFilme(id) {
+        let filtroFilmes = filmes.filter((item) => {
+            return (item.id !== id)
+        })
+
+        setFilmes(filtroFilmes);
+        localStorage.setItem('@j-flix', JSON.stringify(filtroFilmes))
+
+
+    }
+
     return(
 
         <div className="meus-filmes">
             <h1>Meus filmes</h1>
+
+            {filmes.length === 0 && <span> Você não possui nenhum filme salvo :(</span>} 
 
             <ul>
                 {filmes.map((item) => {
@@ -28,7 +41,7 @@ function Favoritos() {
                             <span>{item.title}</span>
                             <div>
                                 <Link to={`/filme/${item.id}`}>Ver detalhes</Link>
-                                <button>Excluir</button>
+                                <button onClick={() => excluirFilme(item.id)}>Excluir</button>
                             </div>
                         </li>
                        
