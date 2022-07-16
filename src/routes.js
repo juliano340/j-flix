d494fe4cf644833a96fc78d894b4a5d41387d336
juliano340 <juliano340@gmail.com>
2022-07-15 22:53:06 -0300

    Criado o component Header

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from './Pages/Home'
 import Filme from './Pages/Filme'
+import Header from "./components/Header";
 
 function RoutesApp() {
     return (
 
         <BrowserRouter>
-        
+        <Header/>
         <Routes>
             <Route path="/" element={<Home/>}/>
             <Route path="/filme/:id" element={<Filme/>}/>
